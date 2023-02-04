58ea11258f455552cf6c811832ac14fa8e62096e
josep93 <joseluisgomezmino@hotmail.com>
2023-02-04 19:11:28 +0100

    Updated round controller

--- Assets/Scripts/Round/RoundEvent.cs
+++ Assets/Scripts/Round/RoundEvent.cs
@@ -8,13 +8,39 @@ public class RoundEvent : MonoBehaviour
     private RoundScript roundScript;
     private Camera cam;
 
-    [SerializeField] float zPos = -10f;
-    [SerializeField] float tVal = 0.2f;
+
+    [Header("Controlador de movimiento")]
+    [SerializeField] private bool moving = false;
+
+    [Header("Variables de posición")]
+    [SerializeField] float speedPosticion = 0f;
+    [SerializeField] float xPosticion = 0f;
+    [SerializeField] float yPosticion = 0f;
+
+    [Header("Variables de rotación")]
+    [SerializeField] float speedRotation = 0f;
+    [SerializeField] float camRotation = 0f;
+
+    [Header("Variables de efoque")]
+    [SerializeField] float camSize = 10.75f;
+    [SerializeField] float speedSize = 0.5f;
+
 
     private void Start()
     {
         roundScript = this.GetComponentInParent<RoundScript>();
         cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (!moving) { return; }
+        cam.transform.SetPositionAndRotation(
+            Vector3.MoveTowards(cam.transform.position, // Current position
+            new Vector3(xPosticion, yPosticion, cam.transform.position.z), speedPosticion), // New position, speed
+            Quaternion.Slerp(cam.transform.rotation, Quaternion.Euler(0, 0, camRotation), speedRotation)  // Rotation
+            );
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize, speedSize);  // Tamaño cámara
     }
 
     public enum Eventos : byte
@@ -41,19 +67,52 @@ public class RoundEvent : MonoBehaviour
         }
     }
 
-    #region LabRound
-    IEnumerator LabRound()
+    private void RestartVar()
     {
-        float i = 10;
+        // Move
+        speedPosticion = 0f;
+        xPosticion = 0f;
+        yPosticion = 0f;
+
+        // Rotation
+        speedRotation = 0;
+        camRotation = 0;
+
+        // Size
+        camSize = 10.75f;
+        speedSize = 0;
+
+        moving = false;
+        roundScript.EndRound();
+    }
+
+    #region LabRound
+    IEnumerator LabRound(float speedPosticion = 0.1f, float speedRotation = 0.2f, float speedSize = 0.0125f)
+    {
+        moving = true;
+        xPosticion = 5;
+        yPosticion = -5;
+        this.speedPosticion = speedPosticion;
+
+        this.speedRotation = speedRotation;
+        camRotation = -10f;
+
+        camSize = 8f;
+        this.speedSize = speedSize;
+        yield return new WaitForSeconds(2);
+        RestartVar();
+
+
+        /*float i = 10;
         while (i > 0)
         {
-            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, Quaternion.Euler(0, 0, zPos), tVal);
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, Quaternion.Euler(0, 0, camRotation), speedRotation);
             i -= 0.25f;
             yield return new  WaitForSeconds(0.01f);
         }
 
         yield return null;
-        roundScript.EndRound();
+        roundScript.EndRound();*/
     }
     #endregion
 
